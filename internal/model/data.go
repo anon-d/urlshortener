5645d28281587699fe813e6b3727cc2cc36dce83
anon-d <fas_ufa02@icloud.com>
2025-12-18 14:33:19 +0300

    Fix properties naming in model.data

--- internal/model/data.go
+++ internal/model/data.go
@@ -1,14 +1,14 @@
 package model
 
 type Data struct {
-	Id          string `json:"uuid"`
+	ID          string `json:"uuid"`
 	ShortURL    string `json:"short_url"`
 	OriginalURL string `json:"original_url"`
 }
 
 func NewData(id, short, original string) Data {
 	return Data{
-		Id:          id,
+		ID:          id,
 		ShortURL:    short,
 		OriginalURL: original,
 	}
